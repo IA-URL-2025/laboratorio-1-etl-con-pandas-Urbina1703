e84c9a46b82a6d436e3d03b07711291d4dace6bb
Diego <djesusurbinachavez@gmail.com>
2026-01-22 15:00:45 -0600

    Implementacion ETL con pandas

--- src/etl.py
+++ src/etl.py
@@ -1,12 +1,28 @@
 import pandas as pd
 
 def run_etl():
-    """
-    Implementa el proceso ETL.
-    No cambies el nombre de esta función.
-    """
-    # TODO: implementar
-    pass
+    # Extraer datos
+    df = pd.read_csv("data/citas_clinica.csv")
+
+    # Transformar datos
+
+    
+    df["paciente"] = df["paciente"].str.title()
+    df["especialidad"] = df["especialidad"].str.upper()
+
+    
+    df["fecha_cita"] = pd.to_datetime(df["fecha_cita"], errors="coerce")
+    df = df.dropna(subset=["fecha_cita"])
+
+    
+    df = df[df["estado"] == "CONFIRMADA"]
+    df = df[df["costo"] > 0]
+
+    
+    df["telefono"] = df["telefono"].fillna("NO REGISTRA")
+
+    # Cargar los datos
+    df.to_csv("data/output.csv", index=False)
 
 
 if __name__ == "__main__":
